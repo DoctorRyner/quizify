aa871ab9b205f59a0874c710fde069a0ca9ad238
drryner <drryner@icloud.com>
2019-03-07 16:27:21 +0500

    Release

--- src/Main.hs
+++ src/Main.hs
@@ -6,25 +6,25 @@ import qualified Config
 
 quizify :: String -> Bool -> FilePath -> FilePath -> IO ()
 quizify accentColor isDark imageSource dist = do
-    let distMask = dist ++ if last dist /= '/' then "/*.{jpg,png}" else "*.{jpg,png}"
-        cmd = concat
-            [ "mkdir -p " ++ dist ++ if last dist /= '/' then " && " else "/ && "
-            , "cp -r " ++ imageSource ++ if last imageSource /= '/' then "/ " else " " ++ dist
+    let distMask = dist ++ "/*.{jpg,png}"
+        cmd = init . init . init $ concatMap (++ " && ")
+            [ "echo 'Replacing colors, output to " ++ dist ++ " ...'"
+            , "mkdir -p " ++ dist ++ "/"
+            , "cp -r " ++ imageSource ++ "/ " ++ dist
             , "mogrify -fill "
                 ++ accentColor
                 ++ " -opaque "
-                ++ Config.darkColor
+                ++ Config.lightColor ++ " "
                 ++ distMask
             , if not isDark
                 then "mogrify -fill "
                     ++ Config.darkColor
                     ++ " -opaque "
-                    ++ Config.lightColor
+                    ++ Config.lightColor ++ " "
                     ++ distMask
-                else ""
+                else "echo 'FINISH!'"
             ]
-    _ <- system cmd
-    pure ()
+    system cmd >> pure ()
 
 {-
 replaceAccentColor: mogrify -fill <accentColor> -opaque <light> <distPath>*.{jpg,png}
@@ -36,15 +36,16 @@ replaceLightToDark: mogrify -fill <dark> -opaque <light> <distPath>*.{jpg,png}
 main :: IO ()
 main = do
     args <- getArgs
+    let accentColor = "'" ++ head args ++ "'"
     case length args of
-        4 -> quizify (head args) (args !! 1 == "dark") (args !! 2) (args !! 3)
-        3 -> quizify (head args) (args !! 1 == "dark") "images" (args !! 2)
-        2 -> quizify (head args) True "images" (args !! 1)
+        4 -> quizify accentColor (args !! 1 == "dark") (args !! 2) (args !! 3)
+        3 -> quizify accentColor (args !! 1 == "dark") "images" (args !! 2)
+        2 -> quizify accentColor True "images" (args !! 1)
         1 ->
             let arg = head args
             in if any (== arg) [ "help", "-help", "--help" ]
                 then showHelp
-                else quizify (args !! 0) True "images" "dist"
+                else quizify accentColor True "images/" "dist/"
         _ -> showHelp
 
   where showHelp = putStrLn "Usage examples:\n\n4 args: quizify #fff light images/ dist/\n3 args: quizify #fff light dist/\n2 args: quizify #fff dist/\n1 arg:\n  - quizify #fff\n  - quizify help\n  - quizify -help\n  - quizify --help\n\nwhere:\n  - #fff: changeable accent color\n  - light: quiz theme, can be light or dark\n  - images/: source img path\n  - dist/: output directory"
