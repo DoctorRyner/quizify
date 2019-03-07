39b6caa8ff736285b3368d53bf9e2ce7126893e1
drryner <drryner@icloud.com>
2019-03-07 16:38:10 +0500

    Some refactoring

--- src/Config.hs
+++ src/Config.hs
@@ -2,8 +2,8 @@ module Config where
 
 -- It's default (dark) cards color
 darkColor :: String
-darkColor = "'#4A4A4A'"
+darkColor = "#4A4A4A"
 
 -- It's light variation of cards color
 lightColor :: String
-lightColor = "'#DADADA'"
+lightColor = "#DADADA"
--- src/Main.hs
+++ src/Main.hs
@@ -4,48 +4,36 @@ import System.Environment
 import System.Process (system)
 import qualified Config
 
+mogrify :: String -> String -> String -> String
+mogrify targetColor srcColor mask =
+    "mogrify -fill " ++ "'" ++ targetColor ++ "'" ++ " -opaque " ++ "'" ++ srcColor ++ "' " ++ mask
+
 quizify :: String -> Bool -> FilePath -> FilePath -> IO ()
-quizify accentColor isDark imageSource dist = do
-    let distMask = dist ++ "/*.{jpg,png}"
-        cmd = init . init . init $ concatMap (++ " && ")
-            [ "echo 'Replacing colors, output to " ++ dist ++ " ...'"
-            , "mkdir -p " ++ dist ++ "/"
-            , "cp -r " ++ imageSource ++ "/ " ++ dist
-            , "mogrify -fill "
-                ++ accentColor
-                ++ " -opaque "
-                ++ Config.lightColor ++ " "
-                ++ distMask
-            , if not isDark
-                then "mogrify -fill "
-                    ++ Config.darkColor
-                    ++ " -opaque "
-                    ++ Config.lightColor ++ " "
-                    ++ distMask
-                else "echo 'FINISH!'"
-            ]
-    system cmd >> pure ()
-
-{-
-replaceAccentColor: mogrify -fill <accentColor> -opaque <light> <distPath>*.{jpg,png}
-
-replaceDarkToLight: mogrify -fill <light> -opaque <dark> <distPath>*.{jpg,png}
-replaceLightToDark: mogrify -fill <dark> -opaque <light> <distPath>*.{jpg,png}
--}
+quizify accentColor isDark imageSource dist = system cmd >> pure ()
+  where
+    distMask = dist ++ "/*.{jpg,png}"
+    cmd = init . init . init $ concatMap (++ " && ")
+        [ "echo 'Replacing colors, output to " ++ dist ++ " ...'"
+        , "mkdir -p " ++ dist ++ "/"
+        , "cp -r " ++ imageSource ++ "/ " ++ dist
+        , mogrify accentColor Config.lightColor distMask
+        , if not isDark
+            then mogrify Config.darkColor Config.lightColor distMask
+            else "echo 'FINISH!'"
+        ]
 
 main :: IO ()
 main = do
     args <- getArgs
-    let accentColor = "'" ++ head args ++ "'"
     case length args of
-        4 -> quizify accentColor (args !! 1 == "dark") (args !! 2) (args !! 3)
-        3 -> quizify accentColor (args !! 1 == "dark") "images" (args !! 2)
-        2 -> quizify accentColor True "images" (args !! 1)
+        4 -> quizify (head args) (args !! 1 == "dark") (args !! 2) (args !! 3)
+        3 -> quizify (head args) (args !! 1 == "dark") "images" (args !! 2)
+        2 -> quizify (head args) True "images" (args !! 1)
         1 ->
             let arg = head args
             in if any (== arg) [ "help", "-help", "--help" ]
                 then showHelp
-                else quizify accentColor True "images/" "dist/"
+                else quizify (head args) True "images/" "dist/"
         _ -> showHelp
 
   where showHelp = putStrLn "Usage examples:\n\n4 args: quizify #fff light images/ dist/\n3 args: quizify #fff light dist/\n2 args: quizify #fff dist/\n1 arg:\n  - quizify #fff\n  - quizify help\n  - quizify -help\n  - quizify --help\n\nwhere:\n  - #fff: changeable accent color\n  - light: quiz theme, can be light or dark\n  - images/: source img path\n  - dist/: output directory"
